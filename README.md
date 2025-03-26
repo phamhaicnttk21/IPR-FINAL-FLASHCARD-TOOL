006a5ae23eb821fafa1cb58542d74635287c6b3a
ngoc-anh22 <anhtran.tn03@gmail.com>
2025-03-26 10:31:55 +0700

    Post_uploaded file process

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Guide to start
 2. Install appropriate package : pip install fastapi uvicorn pandas openpyxl gTTS
 3. Run commmand : pip install -q -U google-genai
 4. Start app : uvicorn main:app --reload
-5. Access api doc locally: 127.0.0.8080/docs 
+5. Access api doc locally: 127.0.0.8000/docs 
 
 
 
@@ -13,3 +13,12 @@ Guide to Test feature Prompt AI to generate word:
 2. Run command : python prompt_service.py
 
 
+Guide to test post_uploaded process:
+1. Start app : uvicorn main:app --reload
+2. Open postman run post request (http://127.0.0.1:8000/home/processData)
+3. Enter body raw with this content:
+{
+    "filename": "Book1.xlsx"
+}
+
+4. Output will appear in the processed folder
--- app/routes/file_router.py
+++ app/routes/file_router.py
@@ -1,14 +1,18 @@
-from fastapi import APIRouter, File, UploadFile, HTTPException
+from fastapi import APIRouter, File, UploadFile, HTTPException, Body
 from pathlib import Path
 
 from pydantic import BaseModel
 
 from app.services.file_service import validate_and_save_file,delete_file,update_file
+from app.services.post_uploaded import *
 
 router = APIRouter()
 
 UPLOAD_DIR = Path("uploads")
 UPLOAD_DIR.mkdir(exist_ok=True)  # Ensure the upload directory exists
+
+PROCESS_DIR = Path("processed")
+PROCESS_DIR.mkdir(exist_ok=True) # Ensure the processed directory exists
 
 
 @router.post("/uploadDoc")
@@ -26,3 +30,16 @@ class UpdateRequest(BaseModel):
 @router.put("/updateDoc")
 async def update_doc(filename: str, request: UpdateRequest):
     return update_file(filename, request.updates, UPLOAD_DIR)
+
+@router.post("/processData")
+async def process_data(filename: str = Body(embed=True)):
+    try:
+        file_path = Path("uploads") / filename  # Create the full path.
+        data_dict = data_to_dict(str(file_path))
+        save_dict_to_json(data_dict, filename)
+        return data_dict
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Internal Server Error: {e}")
+
